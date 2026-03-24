70b28bf570a51de51298edfa6a22096e25e17def
FirstName LastNamegit config --global user.name Grace <grace.sshang@gmail.com>
2026-03-24 16:51:02 -0700

    written instruction + more work on AI summary (tried onload in js)

--- src/routes/summarize.ts
+++ src/routes/summarize.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import { summarizeNote } from '../controllers/summarizeController.js';
 
 const router = express.Router();
+// api/summarize/:petid/:notetype
+//in the json body for API have {prompt: "" summarize last week}
 
+//route call controller, controller call services, services is where you call observernoteservice (getobservernotesbyid), send that to AI and return the summary
+//use postman?
 router.post('/summarize', summarizeNote);
 
 export default router;
