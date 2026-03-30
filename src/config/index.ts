0a475fc1adf88591355b3981641d4313582f364a
Nofees4Novembah <nafis.mahmud.rahman@gmail.com>
2026-03-30 15:45:24 -0700

    Wrote the implementation of observerNotesDB

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 dotenv.config();
 
 export default {
@@ -16,3 +18,16 @@ export default {
     bearer: process.env.RESCUE_GROUPS_BEARER ?? "",
   },
 };
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION ?? "us-east-1",
+  ...(process.env.AWS_ENDPOINT && {
+    endpoint: process.env.AWS_ENDPOINT ?? "http://localhost:4566",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "test",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "test",
+    },
+  }),
+});
+
+export const docClient = DynamoDBDocumentClient.from(client);
