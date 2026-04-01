f50aa19736a94ad0c32f6bad6790e8e5cc2aac94
Haotian2006 <haotian2090@gmail.com>
2026-03-31 17:15:45 -0700

    remove aws config

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,12 +7,6 @@ export default {
   port: Number(process.env.PORT) || 3000,
   USE_MOCK_RG_DB: process.env.USE_MOCK_RG_DB === "true",
   USE_MOCK_NOTES_DB: process.env.USE_MOCK_NOTES_DB === "true",
-  aws: {
-    region: process.env.AWS_REGION ?? "us-east-1",
-    endpoint: process.env.AWS_ENDPOINT ?? "http://localhost:4566",
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "test",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "test",
-  },
   rescueGroups: {
     endpoint: process.env.RESCUE_GROUPS_ENDPOINT ?? "",
     bearer: process.env.RESCUE_GROUPS_BEARER ?? "",
