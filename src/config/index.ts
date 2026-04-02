7f068fc1939e01faa6e0e00d204b84c750875f3f
Haotian2006 <haotian2090@gmail.com>
2026-04-02 13:43:53 -0700

    added aws back

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,10 +10,16 @@ export default {
   LLM_API_KEY: process.env.OPENAI_API_KEY ?? "",
 
   USE_MOCK_NOTES_DB: process.env.USE_MOCK_NOTES_DB === "true",
- 
+
   rescueGroups: {
     endpoint: process.env.RESCUE_GROUPS_ENDPOINT ?? "",
     bearer: process.env.RESCUE_GROUPS_BEARER ?? "",
+  },
+  aws: {
+    region: process.env.AWS_REGION ?? "us-east-1",
+    endpoint: process.env.AWS_ENDPOINT ?? "http://localhost:4566",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "test",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "test",
   },
 };
 
