98ea82c462d5acfda55f1622eca08a5ce8f5afd2
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2026-04-17 00:59:05 -0700

    update create all tables

--- src/db/scripts/createAllTables.ts
+++ src/db/scripts/createAllTables.ts
@@ -2,12 +2,18 @@ import { main as createObserverNotesTable } from "./createObserverNotesTable.js"
 import { main as createBehaviorNotesTable } from "./createBehaviorNotesTable.js";
 import { main as createPetCompatibilityTable } from "./createPetCompatibilityTable.js";
 import { main as createMergedNotesTable } from "./createMergedNotesTable.js";
+import { main as createUsersTable } from "./createUsersTable.js";
+import { main as createRefreshTokensTable } from "./createRefreshTokensTable.js";
+import { main as createActivityLogTable } from "./createActivityLogTable.js";
 
 export const main = async () => {
   await createObserverNotesTable();
   await createBehaviorNotesTable();
   await createPetCompatibilityTable();
   await createMergedNotesTable();
+  await createUsersTable();
+  await createRefreshTokensTable();
+  await createActivityLogTable();
 };
 
 main().catch((error) => {
